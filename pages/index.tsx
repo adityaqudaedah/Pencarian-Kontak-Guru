07b0eebc9176dd5f2747ec371e42f71abbfd122f
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2022-03-02 19:45:59 +0700

    fix user auth bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Layout from "../components/layout";
 // import { Box } from "@chakra-ui/react";
@@ -7,18 +8,51 @@ import ListItems from "../components/listItem";
 import Logo from "../components/logo";
 import Me from "../components/me";
 import { useAppSelector } from "../hooks/useAppSelector";
+import { getAuth } from "firebase/auth";
 
 const Home: NextPage = () => {
   const userAuthState = useAppSelector((state) => state.userAuth.value);
-  console.log(userAuthState);
+  const [user, setUser] = useState<boolean>(false);
+  const [isUserExist, setIsUserExist] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("user") === null) {
+      console.log("user not exist");
+      setIsUserExist(false);
+    } else {
+      console.log("user exist");
+      setIsUserExist(true);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const { currentUser: user } = getAuth();
+
+    if (user !== null) {
+      localStorage.setItem("user", user.uid);
+      setUser(true);
+    } else {
+      setUser(false);
+    }
+  }, [userAuthState]);
+
   return (
-    <Layout>
-      <Container>
-        {userAuthState?.uid.length!==0 && <Me />}
-        {userAuthState?.uid.length===0&&<><Logo /><InputSearch /><ListItems /></>}
-      </Container>
-        
-    </Layout>
+    <>
+      {isUserExist && (
+        <Container>
+          <Me />
+        </Container>
+      )}
+      {!isUserExist && (
+        <Layout>
+          <Container>
+            <Logo />
+            <InputSearch />
+            <ListItems />
+          </Container>
+        </Layout>
+      )}
+    </>
   );
 };
 
