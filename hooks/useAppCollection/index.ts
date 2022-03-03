a360a596c0488c1d44cf1fc39e30f38710c6d838
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2022-03-03 12:23:13 +0700

    fix bugs collection

--- hooks/useAppCollection/index.ts
+++ hooks/useAppCollection/index.ts
@@ -11,32 +11,53 @@ interface UserDataInterface {
   url: string;
 }
 
-interface UsersDataInterface extends Array<UserDataInterface> {}
+// interface UsersDataInterface extends Array<UserDataInterface> {}
 
 export const useAppCollection = () => {
-  const [users, setUsers] = useState<UsersDataInterface[] | null>(null);
+  const [users, setUsers] = useState<UserDataInterface[] | null>(null);
+  const [collectionExist, setCollectionExist] = useState<boolean>(false)
   let temp: Array<any> = [];
 
   //get data or collection from firestore
   const getDoc = async () => {
+
+    
     const querySnapShot = await getDocs(collection(firebaseApp, "users"));
 
-    //assign temp
+    // console.log(querySnapShot.docs)
+
+    if (querySnapShot.docs.length!==0) {
+      console.log('this function executed')
+       //assign temp
     querySnapShot.forEach((doc) => temp.push(doc.data()));
 
     //update users state
     setUsers(temp);
+      // setCollectionExist(true)
+      return true
+    }
+   return false
   };
 
   useEffect(() => {
     let identifier: ReturnType<typeof setTimeout>
     
-    identifier = setTimeout(()=> getDoc(),100)
+      console.log('executed')
+       identifier = setTimeout(() => { getDoc()},1000)
+    
+   
    
     return () => {
+      console.log('clean up')
       clearTimeout(identifier)
     };
-  }, []);
+  }, [collectionExist]);
+
+  useEffect(() => {
+
+    setCollectionExist(true)
+  }, [users])
+  
 
   return users;
 };
